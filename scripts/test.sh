28bc874f9beefe68a580b431caa15d20e1f8dbfa
RevanthTiruveedhi <Revanth.Tiruveedhi@gmail.com>
2021-09-23 08:09:04 +0200

    first commit

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 . /appenv/bin/activate
 
-pip install -r requirements_test.txt
+pip3 download -d /build -r requirements_test.txt --no-input
+
+pip3 install --no-index -f /build -r requirements_test.txt
 
 exec $@
---
+++ src/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                        =   "todobackend",
+    version                     =   "0.1.0",
+    description                 =   "Todobackend Django REST service",
+    packages                    =   find_packages(),
+    include_package_data        =   True,
+    scripts                     =   ["manage.py"],
+    install_requires            =   ["Django>=1.9,<2.0",
+                                     "django-cors-headers>=1.1.0",
+                                     "djangorestframework>=3.3.0",
+                                     "mysql-connector-python>=8.0.26",
+                                     "mysqlclient>=2.0.3"],
+    extras_require              =   {
+                                        "test": [
+                                            "colorama>=0.4.4",
+                                            "coverage>=5.5",
+                                            "django-nose>=1.4.7",
+                                            "nose>=1.3.7",
+                                            "pinocchio>=0.4.3",
+                                            "protobuf>=3.17.3",
+                                            "PyMySQL>=1.0.2",
+                                            "six>=1.16.0"
+                                        ]
+
+                                    }
+
+)
