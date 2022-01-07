5124d598fcfacd614d9d0622434540a7ebfca20a
RevanthTiruveedhi <Revanth.Tiruveedhi@gmail.com>
2022-01-07 19:58:48 +0100

    first commit1

--- src/todobackend/settings/test.py
+++ src/todobackend/settings/test.py
@@ -23,7 +23,7 @@ DATABASES = {
         'NAME': os.environ.get('MYSQL_DATABASE','todobackend'),
         'USER': os.environ.get('MYSQL_USER','todo'),
         'PASSWORD': os.environ.get('MYSQL_PASSWORD','password'),
-        'HOST': os.environ.get('MYSQL_HOST','34.88.11.93'),
+        'HOST': os.environ.get('MYSQL_HOST','localhost'),
         'PORT': os.environ.get('MYSQL_PORT','3306'),
     }
 }
